a9f922286c9e9427629507b18deee2a47216ea7d
senindu003 <senindudinapura@gmail.com>
2025-12-22 13:43:30 +0530

    auth.py changed

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+ACCESS_TOKEN_EXPIRE_MINUTES = 60*24
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
